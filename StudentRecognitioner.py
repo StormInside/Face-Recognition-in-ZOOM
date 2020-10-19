02fb544a7a3a88bc3b8bf11ee3c4c47c9471cd2e
Yurii Agafonov <storminside4766@gmail.com>
2020-10-19 13:23:27 +0300

    1

--- StudentRecognitioner.py
+++ StudentRecognitioner.py
@@ -10,7 +10,7 @@ class StudentRecognitioner:
     def __init__(self, known_pictures_location):
 
         known_pictures = self._get_known_pictures(known_pictures_location)
-        self.known_encodings =  self._get_known_encodings()
+        self.known_encodings =  self._get_known_encodings(known_pictures)
 
 
     def _get_known_pictures(self, path):
@@ -39,9 +39,11 @@ class StudentRecognitioner:
         for pic in known_pictures:
             known_image = fr.load_image_file(pic)
             face_encodings = fr.face_encodings(known_image)
-            print(face_encodings)
-            known_encoding = fr.face_encodings(known_image)[0]
-            relations.update({pic.replace(".jpg","").replace(".png","") : known_encoding})
+            if face_encodings:
+                known_encoding = fr.face_encodings(known_image)[0]
+                relations.update({pic.replace(".jpg","").replace(".png","") : known_encoding})
+            else:
+                print("Faces not found in ", pic)
 
         if relations: 
             return relations
@@ -62,8 +64,8 @@ class StudentRecognitioner:
             matches = fr.compare_faces(list(self.known_encodings.values()), face_encoding)
 
             face_distances = fr.face_distance(list(self.known_encodings.values()), face_encoding)
-            print(self.known_encodings)
-            print(face_distances)
+            # print(self.known_encodings)
+            # print(face_distances)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 
